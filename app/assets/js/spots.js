089c8913b4d9a51615ee21af99d1be5774188cd8
jimmyFang-ai <pi20120413@yahoo.com.tw>
2022-06-06 18:02:13 +0800

    git init

--- app/assets/js/spots.js
+++ app/assets/js/spots.js
@@ -4,11 +4,12 @@ const searchCategory = document.querySelector(".search-category");
 
 const categoryList = document.querySelector(".spots-categoryList");
 
+console.log(searchCategory);
 if (categoryList) {
     categoryList.addEventListener("click", function (e) {
         e.preventDefault();
 
-        let category = e.target.closest(".spots-card");
+        let category = e.target.closest(".category-card");
         let categoryVal = e.target.closest("li").dataset.category;
 
         searchCategory.value = categoryVal;
--- app/assets/js/swiper.js
+++ app/assets/js/swiper.js
@@ -1,12 +1,12 @@
-// 首頁 heroBanner 
-const swiperHeroBanner = new Swiper(".swiper-heroBanner", {
+// 首頁 - heroBanner 
+const swiper_heroBanner = new Swiper(".swiper-heroBanner", {
     cssMode: true,
     slidesPerView: 1,
-    // loop: true,
-    // autoplay: {
-    //   disableOnInteraction: false,
-    //   delay: 4000,
-    // },
+    loop: true,
+    autoplay: {
+      disableOnInteraction: false,
+      delay: 4000,
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -18,8 +18,8 @@ const swiperHeroBanner = new Swiper(".swiper-heroBanner", {
   });
   
 
-// 熱門打卡景點
-const swiperSpots = new Swiper(".swiper-spots", {
+// 首頁 - 熱門打卡景點
+const swiperSpots = new Swiper(".swiper-homeSpots", {
   slidesPerView: 1.6,
   spaceBetween: 16,
   grid: {
@@ -46,8 +46,78 @@ const swiperSpots = new Swiper(".swiper-spots", {
   }
 });
 
-// 一再回訪美食
-const foodSpots = new Swiper(".swiper-foods", {
+// 首頁 - 一再回訪美食
+const swiperFoods = new Swiper(".swiper-homeFoods", {
+  slidesPerView: 1.6,
+  spaceBetween: 16,
+  grid: {
+      rows: 1,
+      fill: 'row',
+  },
+  breakpoints:{
+    992: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        grid: {
+            rows: 1,
+            fill: 'row',
+        },
+    },
+    768: {
+        slidesPerView: 2.5,
+        spaceBetween: 30,
+        grid: {
+            rows: 1,
+            fill: 'row',
+        },
+    },
+  }
+});
+
+
+
+// 探索景點 內頁 swiper
+const swiper_spotsBanner = new Swiper(".swiper-spotsBanner", {
+  cssMode: true,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    disableOnInteraction: false,
+    delay: 4000,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+
+// 節慶活動 內頁 swiper
+const swiper_activityBanner = new Swiper(".swiper-activityBanner", {
+  cssMode: true,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    disableOnInteraction: false,
+    delay: 4000,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+
+// 推薦商品  swiper
+const swiperRecommend = new Swiper(".swiper-recommend", {
   slidesPerView: 1.6,
   spaceBetween: 16,
   grid: {
