5da3681e5dbec33decfbc5709312a82bc8ec136a
jimmyFang-ai <pi20120413@yahoo.com.tw>
2022-06-09 20:20:55 +0800

    git init

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,11 +1,33 @@
+// header 驗證
+function getAuthorizationHeader() {
+    //  填入自己 ID、KEY 開始
+    let AppID = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
+    let AppKey = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
+    //  填入自己 ID、KEY 結束
+    let GMTString = new Date().toGMTString();
+    let ShaObj = new jsSHA('SHA-1', 'TEXT');
+    ShaObj.setHMACKey(AppKey, 'TEXT');
+    ShaObj.update('x-date: ' + GMTString);
+    let HMAC = ShaObj.getHMAC('B64');
+    let Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
+    return { 'Authorization': Authorization, 'X-Date': GMTString };
+}
 
-// jq 初始化
-$(function () {
-  console.log("QQ");
-})
+
+// apiUrl
 
 
-// function init() {
 
-//   // AOS.init();
-// };
+axios.get(
+    'https://ptx.transportdata.tw/MOTC/v2/Rail/TRA/Station?$top=10&$format=JSON',
+    {
+        headers: getAuthorizationHeader()
+    }
+)
+    .then(function (response) {
+        //    document.querySelector('body').textContent=JSON.stringify(response.data);
+       console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error.response.data);
+    });
