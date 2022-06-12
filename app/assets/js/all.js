d9a4d15b15d4e7c4a717cc2d46a87b433d4761fd
jimmyFang-ai <pi20120413@yahoo.com.tw>
2022-06-12 13:54:36 +0800

    git init

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,7 @@
+// 帳號未註冊完成: 可不使用API金鑰呼叫API，但每日呼叫次數上限為50次。
+
+
+
 // header 驗證
 function getAuthorizationHeader() {
     //  填入自己 ID、KEY 開始
@@ -14,23 +18,41 @@ function getAuthorizationHeader() {
 }
 
 
+
 // apiUrl
-// scenicSpot_apiUrl
+let apiUrl_scenicSpot = 'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/?$filter=Picture/PictureUrl1 ne null&$format=JSON';
+
 // activity_apiUrl
 // restaurant_apiUrl
 
 
+// 初始化
+function init() {
+    get_ScenicSpot();
+}
+init();
 
-axios.get(
-    'https://ptx.transportdata.tw/MOTC/v2/Rail/TRA/Station?$top=10&$format=JSON',
-    {
-        headers: getAuthorizationHeader()
-    }
-)
-    .then(function (response) {
-        //    document.querySelector('body').textContent=JSON.stringify(response.data);
-       console.log(response.data);
-    })
-    .catch(function (error) {
-        console.log(error.response.data);
-    });
+
+// 觀光景點: 取得資料 & 渲染畫面
+function get_ScenicSpot() {
+    axios.get(apiUrl_scenicSpot,
+        {
+            headers: getAuthorizationHeader()
+        }
+    )
+        .then(function (response) {
+            //    document.querySelector('body').textContent=JSON.stringify(response.data);
+            console.log(response.data);
+            let thisData = response.data;
+            // 將沒有三張圖片的資料都濾掉
+            thisData = thisData.filter((item) => {return item.Picture.PictureUrl1 !== undefined && item.Picture.PictureUrl2 !== undefined && item.Picture.PictureUrl3 !== undefined});
+            console.log(thisData);
+  
+            
+        })
+        .catch(function (error) {
+            console.log(error.response.data);
+        });
+
+}
+
