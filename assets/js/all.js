81126e8e77843c5de1a741feb954f52b0df2c8b6
jimmyFang-ai <pi20120413@yahoo.com.tw>
2022-06-28 20:59:33 +0800

    Update 2022-06-28T12:59:30.960Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -419,15 +419,15 @@ $(function () {
 
   restaurant_getAllData(); // 取得 網址參數
 
-  if (location.pathname === '/activity.html') {
+  if (location.pathname === '/activity.html' || location.pathname === '/The-F2E-_-week1-TaiWalk/activity.html') {
     activity_getParameters();
   }
 
-  if (location.pathname === '/scenicSpot.html') {
+  if (location.pathname === '/scenicSpot.html' || location.pathname === '/The-F2E-_-week1-TaiWalk/scenicSpot.html') {
     scenicSpot_getParameters();
   }
 
-  if (location.pathname === '/restaurant.html') {
+  if (location.pathname === '/restaurant.html' || location.pathname === '/The-F2E-_-week1-TaiWalk/restaurant.html') {
     restaurant_getParameters();
   } // loading 動畫
 
